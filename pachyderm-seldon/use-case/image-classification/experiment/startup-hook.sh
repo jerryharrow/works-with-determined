19a45af04fa4f1e02bc5885a33165f727373930d
Jerry J. Harrow <84593277+jerryharrow@users.noreply.github.com>
2023-05-04 12:09:18 -0400

    New PFS branch example with pre-built image
    environments:pach-cuda-11.3-pytorch-1.12-tf-2.8-gpu-9b5db1b

--- pachyderm-seldon/use-case/image-classification/experiment/startup-hook.sh
+++ pachyderm-seldon/use-case/image-classification/experiment/startup-hook.sh
@@ -1,5 +1,2 @@
-echo >&2 "Installing scikit-image"
-pip install scikit-image==0.19.2 python_pachyderm==7.3.0
-
 # Unset proxies so we can use pachyderm API 
 unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
